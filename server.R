80f37fff89f24ae6b7352a0f5eb3afa919f8a75e
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2016-04-11 00:06:41 -0400

    Added comments and cleaned up code.

--- server.R
+++ server.R
@@ -1,26 +1,18 @@
 library(shiny)
 source("customerPredictor.R")
 
-# Define server logic for random distribution application
+# Define server logic for the dashboard.
 shinyServer(function(input, output) {
   
   # Reactive expression to generate the requested distribution.
   # This is called whenever the inputs change. The output
   # functions defined below then all use the value computed from
   # this expression
-  #data <- reactive({
-  # dow <- switch(input$dow,
-  #                 norm = rnorm,
-  #                 unif = runif,
-  #                 lnorm = rlnorm,
-  #                 exp = rexp,
-  #                 rnorm)
-  #  
-  #  dow(input$hour)
-  #})
   
-  #test
+  # Reactive expression called whenever the inputs changed.
+  # Used to calculate the minutes set and the machine learning function.
   data <- reactive({
+    # Create a set based on the two bounds from the minute input
     minutes <- c(input$minute[1])
     if (input$minute[1]+1 < input$minute[2]) {
       for (i in (input$minute[1]+1):input$minute[2]) {
@@ -29,29 +21,31 @@ shinyServer(function(input, output) {
     } else {
       minutes <- append(minutes, input$minute[2])
     }
+    # Transform this vector of minutes into a data frame.
     customerData <- as.data.frame(minutes)
     
-    #predictCustomerAmount(input$dayOfMonth, input$dayOfWeek, input$minute, input$temp, input$prec)
-    customerData$customerCount <- apply(as.data.frame(minutes), 1, function(x) predictCustomerAmount(input$dayOfMonth, input$dayOfWeek, x, input$temp, input$prec))
+    # Apply the function predictCustomerAmount() to every minute in the data frame (where x is the minute)
+    customerData$customerCount <-
+      apply(as.data.frame(minutes), 1, 
+            function(x) predictCustomerAmount(input$dayOfMonth, input$dayOfWeek, x, input$temp, input$prec))
     
+    # predictCustomerAmount calculates the amount of customers over the next 15 minutes from the minute inputted
+    # so divide this number by 15.
     customerData$customerCount <- customerData$customerCount / 15
     
+    # "Output" the data so that it is put into the 'data' variable.
     customerData
   })
-  #data <- as.data.frame(data)
-  #data$Minute <- input$hour*60 + inpute$minute
-  #data$InCount <- data$data
-  #data <- subset(data, select=c("Minute", "InCount"))
   
-  # Generate a plot of the data. Also uses the inputs to build
-  # the plot label. Note that the dependencies on both the inputs
-  # and the data reactive expression are both tracked, and
-  # all expressions are called in the sequence implied by the
-  # dependency graph
+  # Generate a graph of the data that is gathered in the 'data' variable above.
   output$plot <- renderPlot({
+    # Sets the max y-limit for the graph to 10, unless there is greater than 10 customers predicted in one minute.
     maxLim <- ifelse(max(data()$customerCount) > 10, max(data()$customerCount), 10)
+    
+    # Output the plot using the data defined in the reaction function above.
     plot(data(), type="l", ylim=c(1,maxLim), main="Predicted Amount of Customers", xlab="Time", ylab="Amount of Customers")
     
+    # Test stuff
     ticks <- subset(data()$minutes, ((data()$minutes - floor(data()$minutes/60)*60)) %% 15 == 0)
     if (min(ticks) > min(data()$minutes)) {
       ticks <- c(min(ticks) - 15, ticks)
@@ -60,21 +54,12 @@ shinyServer(function(input, output) {
       ticks <- c(ticks, max(ticks) + 15)
     }
     
-  axis(1,at = ticks, labels = paste(floor(ticks/60),":",ticks - (floor(ticks/60)*60),sep = ""))
+    axis(1,at = ticks, labels = paste(floor(ticks/60),":",ticks - (floor(ticks/60)*60),sep = ""))
+    # End test stuff.
   })
   
+  # Sum up all the customers predicted for each minute and output it as text below the plot.
   output$text <- renderText({
     paste(sum(data()$customerCount), " customers")
   })
-  
-  # Generate a summary of the data
-  #output$summary <- renderPrint({
-  #  summary(data())
-  #})
-  
-  # Generate an HTML table view of the data
-  #output$table <- renderTable({
-  #  data.frame(x=data())
-  #})
-  
 })
