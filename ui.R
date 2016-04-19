76e52d631a2ee88b823a53dd0fc44b9c0d9f3cf9
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2016-04-19 01:03:58 -0400

    Added support for user login and sign up.
    Converted page to 2 conditional panels, one for login/sign up pages, the
    other for the predictors. User is only able to view the predictor panel
    when they are "logged in" with a valid username and password
    combination.

--- ui.R
+++ ui.R
@@ -3,66 +3,124 @@ library(shiny)
 # Define UI for the dashboard.
 shinyUI(fluidPage(
   
+  # JavaScript to submit forms on ENTER (specific to these input ids).
+  tags$head(
+    tags$script(src = "formSubmit.js")
+  ),
+  
   # Application title
   titlePanel("Cafe Data Predictor"),
   
-  # Create a panel to contain the tabs for each predictor.
-  tabsetPanel(
-    # Customer predictor: user enters values on the current time and weather and a plot is created containing
-    # the predicted amount of customers.
-    tabPanel("Customer Predictor",
-      # Sidebar inputs used to predict the amount of customers at a given time.
-      sidebarLayout(
-        sidebarPanel(
-          # DayOfWeek input (drop down)
-          selectInput("dayOfWeek", "Day of the week:",
-                       c("Monday" = "MONDAY",
-                         "Tuesday" = "TUESDAY",
-                         "Wednesday" = "WEDNESDAY",
-                         "Thursday" = "THURSDAY",
-                         "Friday" = "FRIDAY",
-                         "Saturday" = "SATURDAY",
-                         "Sunday" = "SUNDAY")),
-          br(),
-          
-          # Temperature input (numeric text box)
-          numericInput("temp", "Temperature (F):",
-                       value = 0),
-          br(),
-          
-          # Precipitation input (drop down)
-          selectInput("prec", "Precipitation:",
-                      c("Clear" = "Clear",
-                        "Cloudy" = "Clouds",
-                        "Drizzling" = "Drizzle",
-                        "Fog" = "Fog",
-                        "Misty" = "Mist",
-                        "Raining" = "Rain",
-                        "Snowing" = "Snow")),
-          br(),
-          
-          # Time input (double-ended datetime slider)
-          # Uses only the time part of R's datetime object POSIXlt in the UTC timezone so that the user's timezone
-          # does not affect the values. Steps the slider for every 5 minutes of time.
-          sliderInput("time",
-                      "Time",
-                      min = strptime("00:00", "%H:%M", tz = "UTC"),
-                      max = strptime("23:59", "%H:%M", tz = "UTC"),
-                      value = c(strptime("11:00", "%H:%M", tz = "UTC"), strptime("13:00", "%H:%M", tz = "UTC")),
-                      step = 60*5,
-                      timeFormat = "%H:%M",
-                      timezone = "+0000") # UTC
-        ),
-        
-        # Create a panel to display both the plot and text outputs defined in the server logic.
+  # Output the user's login status as a div (used with conditionalPanels)
+  textOutput("loggedIn"),
+  
+  # Hide the div created above.
+  # Note: display: 'none' cannot be used as the javascript expressions used by the conditionalPanels below will not be able to read 
+  # the value for loggedIn.
+  tags$script("
+    document.getElementById('loggedIn').style.height = '0';
+    document.getElementById('loggedIn').style.overflow = 'hidden';
+  "),
+  
+  # Displays that the user is logged in and their username.
+  textOutput("loginStatus"),
+  
+  # This panel is only displayed if the user is not logged in (Login screen)
+  conditionalPanel(condition = "output.loggedIn == '0'", # This condition parameter requires a javascript expression.
+    tabsetPanel(
+      # Login page
+      tabPanel("Login",
         mainPanel(
-          plotOutput("plot", width = "100%"),
-          textOutput("text")
+          # Username and password input with a submit button
+         textInput("loginUsername", "Username:"),
+         br(),
+         passwordInput("loginPassword", "Password:"),
+         actionButton("loginButton", "Submit"),
+         br(), br(),
+         # Used to display if the user's credintials were incorrect.
+         textOutput("loginDisplayMessage")
+        )
+      ),
+      # Sign up page
+      tabPanel("Sign Up",
+        mainPanel(
+          textInput("signupUsername", "Username:"),
+          br(),
+          passwordInput("signupPassword1", "Password:"),
+          br(),
+          passwordInput("signupPassword2", "Re-enter password:"),
+          #br(),
+          actionButton("signupButton", "Sign Up"),
+          br(), br(),
+          textOutput("signupDisplayMessage")
         )
       )
-    ),
-    tabPanel("Healthy Predictor"),
-    tabPanel("Advertisement Predictor"),
-    tabPanel("Demographic Predictor")
+    )
+  ),
+  
+  # This panel is only displayed if the user is logged in (Predictors)
+  conditionalPanel(condition = "output.loggedIn == '1'",
+  
+    actionButton("logoutButton", "Logout"),
+
+    # Create a panel to contain the tabs for each predictor.
+    tabsetPanel(
+      # Customer predictor: user enters values on the current time and weather and a plot is created containing
+      # the predicted amount of customers.
+      tabPanel("Customer Predictor",
+        # Sidebar inputs used to predict the amount of customers at a given time.
+        sidebarLayout(
+          sidebarPanel(
+            # DayOfWeek input (drop down)
+            selectInput("dayOfWeek", "Day of the week:",
+                         c("Monday" = "MONDAY",
+                           "Tuesday" = "TUESDAY",
+                           "Wednesday" = "WEDNESDAY",
+                           "Thursday" = "THURSDAY",
+                           "Friday" = "FRIDAY",
+                           "Saturday" = "SATURDAY",
+                           "Sunday" = "SUNDAY")),
+            br(),
+            
+            # Temperature input (numeric text box)
+            numericInput("temp", "Temperature (F):",
+                         value = 0),
+            br(),
+            
+            # Precipitation input (drop down)
+            selectInput("prec", "Precipitation:",
+                        c("Clear" = "Clear",
+                          "Cloudy" = "Clouds",
+                          "Drizzling" = "Drizzle",
+                          "Fog" = "Fog",
+                          "Misty" = "Mist",
+                          "Raining" = "Rain",
+                          "Snowing" = "Snow")),
+            br(),
+            
+            # Time input (double-ended datetime slider)
+            # Uses only the time part of R's datetime object POSIXlt in the UTC timezone so that the user's timezone
+            # does not affect the values. Steps the slider for every 5 minutes of time.
+            sliderInput("time",
+                        "Time",
+                        min = strptime("00:00", "%H:%M", tz = "UTC"),
+                        max = strptime("23:59", "%H:%M", tz = "UTC"),
+                        value = c(strptime("11:00", "%H:%M", tz = "UTC"), strptime("13:00", "%H:%M", tz = "UTC")),
+                        step = 60*5,
+                        timeFormat = "%H:%M",
+                        timezone = "+0000") # UTC
+          ),
+          
+          # Create a panel to display both the plot and text outputs defined in the server logic.
+          mainPanel(
+            plotOutput("plot", width = "100%"),
+            textOutput("text")
+          )
+        )
+      ),
+      tabPanel("Healthy Predictor"),
+      tabPanel("Advertisement Predictor"),
+      tabPanel("Demographic Predictor")
+    )
   )
 ))
